fddf2fb86d07c48f279fa20a65d50be62c985b64
Pratik Raj <pratikrajcpr@gmail.com>
2024-09-28 01:25:06 +0530

    Fix: Comment out Last component to resolve build error

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import Work from './Components/Work'
 import Playreel from './Components/Playreel'
 import Images from './Components/Images'
 import Spread from './Components/Spread'
-import Last from './Components/Last'
+// import Last from './Components/Last'
 import LocomotiveScroll from 'locomotive-scroll';
+
 function App() {
-
   const locomotiveScroll = new LocomotiveScroll();
-
 
   return (
     <div className=' w-full '>
@@ -20,7 +19,7 @@ function App() {
       <Playreel />
       <Images />
       <Spread />
-      <Last />
+      {/* <Last /> */}
     </div>
   )
 }
